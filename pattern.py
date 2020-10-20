0652e8588aed4bac9d6338852fc8fa66e79738a5
AnnleeFores <annleefores@gmail.com>
2020-10-20 17:07:54 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -3,4 +3,4 @@ for num in range(rows):
     for i in range(num):
         print(num, end=" ")  # print number
     # line after each row to display pattern correctly
-    print(" ")
+    print("    ")
